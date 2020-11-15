0912df8e1a3e0fec31bd14e3fee153528ab23719
Ben <monoganog@gmail.com>
2020-11-16 10:01:48 +1100

    Added kens work

---
+++ AR-Geolocation-App-Unity/Assets/_Scripts/AutoPlacementOfObjectsInPlane.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARPlaneManager))]
+public class AutoPlacementOfObjectsInPlane : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject placedPrefab;
+
+    private GameObject placedObject;
+
+    [SerializeField]
+    private ARPlaneManager arPlaneManager;
+
+    void Awake() 
+    {
+        //dismissButton.onClick.AddListener(Dismiss);
+        arPlaneManager = GetComponent<ARPlaneManager>();
+        arPlaneManager.planesChanged += PlaneChanged;
+    }
+
+    private void PlaneChanged(ARPlanesChangedEventArgs args)
+    {
+        if(args.added != null && placedObject == null)
+        {
+            ARPlane arPlane = args.added[0];
+            placedObject = Instantiate(placedPrefab, arPlane.transform.position, Quaternion.identity);
+        }
+    }
+
+    //private void Dismiss() => welcomePanel.SetActive(false);
+}
