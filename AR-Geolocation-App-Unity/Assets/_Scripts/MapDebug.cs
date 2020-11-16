9adfc4f97aa9df472e64d046b3e236e05349679c
Ben <monoganog@gmail.com>
2020-11-16 14:41:33 +1100

    better debug menu

--- AR-Geolocation-App-Unity/Assets/_Scripts/MapDebug.cs
+++ AR-Geolocation-App-Unity/Assets/_Scripts/MapDebug.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mapbox.Unity.Location;
 
 public class MapDebug : MonoBehaviour
 {
+    public GameObject UIDebugPanel;
+
     public GameObject UIButtonToEnterAR;
     public Toggle toggle;
+
+    public Text coordinateText;
+    bool _isInitialized;
 
     bool ShouldEnterARMode = false;
     // Start is called before the first frame update
     void Start()
     {
         UIButtonToEnterAR = GameObject.Find("EnterARButton");
+        LocationProviderFactory.Instance.mapManager.OnInitialized += () => _isInitialized = true;
+
     }
 
     // Update is called once per frame
@@ -21,9 +29,44 @@ public class MapDebug : MonoBehaviour
         
     }
 
+    public void EnableDebugging()
+    {
+        bool currentState = UIDebugPanel.activeSelf;
+        UIDebugPanel.SetActive(!currentState);
+    }
+
     public void EnableARMode()
     {
         bool currentUIState = UIButtonToEnterAR.activeSelf;
         UIButtonToEnterAR.SetActive(!currentUIState);
+    }
+
+
+    ILocationProvider _locationProvider;
+    ILocationProvider LocationProvider
+    {
+        get
+        {
+            if (_locationProvider == null)
+            {
+                _locationProvider = LocationProviderFactory.Instance.DefaultLocationProvider;
+            }
+
+            return _locationProvider;
+        }
+    }
+
+    Vector3 _targetPosition;
+
+
+
+    void LateUpdate()
+    {
+        if (_isInitialized)
+        {
+            var map = LocationProviderFactory.Instance.mapManager;
+            //transform.localPosition = map.GeoToWorldPosition(LocationProvider.CurrentLocation.LatitudeLongitude);
+            coordinateText.text = "Lat: "+ LocationProvider.CurrentLocation.LatitudeLongitude.x.ToString() +" Lon: "+ LocationProvider.CurrentLocation.LatitudeLongitude.y.ToString();
+        }
     }
 }
