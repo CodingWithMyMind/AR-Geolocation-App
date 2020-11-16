c709dcb90a0647a017f151f80fbb4b2e80d85d95
Ben <monoganog@gmail.com>
2020-11-16 15:23:59 +1100

    ok

--- AR-Geolocation-App-Unity/Assets/_Scripts/ARManagerScript.cs
+++ AR-Geolocation-App-Unity/Assets/_Scripts/ARManagerScript.cs
@@ -2,10 +2,16 @@ using Mapbox.Unity.Utilities;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ARManagerScript : MonoBehaviour
 {
+    public GameObject OnBoardPanel;
+    public GameObject TextMessagePanel;
+    public Text txtM;
+   
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,5 +35,44 @@ public class ARManagerScript : MonoBehaviour
         Debug.Log("Entering AR Scene");
         SceneManager.LoadScene("MapSceneExperimental", LoadSceneMode.Single);
     }
-    
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartARScene2()
+    {
+        Invoke("StartARScene", 5);
+        DisplayStatus("Start in 5", 5);
+    }
+
+    public void StartARScene()
+    {
+        OnBoardPanel.SetActive(false);
+        
+    }
+
+    public void QuitARScene()
+    {
+        Invoke("Quit", 3);
+        DisplayStatus("Thank you for using our software!", 3);
+    }
+
+    public void DisplayStatus(string Status, int seconds)
+    {
+        
+        txtM.text = Status;
+        TextMessagePanel.SetActive(true);
+        Invoke("HideStatus", seconds);
+    }
+
+    public void HideStatus()
+    {
+        TextMessagePanel.SetActive(false);
+    }
+
+
+
+
 }
