c01d51f04f86db2aa3967bdc6e7002999048fd09
Ken Lau <ksla@deakin.edu.au>
2020-11-23 14:09:08 +1100

    Added Desapwn AfterTime
    GameObject spawn from indicator  will despawn after 5seconds

--- AR-Geolocation-App-Unity/Assets/_Scripts/SphereSpawner.cs
+++ AR-Geolocation-App-Unity/Assets/_Scripts/SphereSpawner.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class SphereSpawner : MonoBehaviour
 {
-
-    public GameObject SpawnObject;
+    [SerializeField]
+    private GameObject SpawnObject;
     private PlacementIndicator placementIndicator;
     
+
+    private float destroyafterSeconds = 5.0F;
+    private float spawnfrequency = 1.0f;
+    private float timer = 0;
+    
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +25,34 @@ public class SphereSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        /*if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
            {
             GameObject obj = Instantiate(SpawnObject,
                 placementIndicator.transform.position, placementIndicator.transform.rotation);
             
+        }*/
+
+        /* testing in editor 
+        if (timer >= spawnfrequency)
+        {
+            GameObject obj = Instantiate(SpawnObject,
+            placementIndicator.transform.position, placementIndicator.transform.rotation);
+
+            Destroy(obj, destroyafterSeconds);
+            timer = 0;
         }
+        timer += Time.deltaTime * 1.0f; */
     }
+
+    public void PlaceObject()
+    {
+        GameObject ObjectA = Instantiate(SpawnObject, placementIndicator.transform.position, placementIndicator.transform.rotation);
+            Destroy(ObjectA, destroyafterSeconds);
+        }
+
+
+
+
 
 
 }
