97608f4b1795d848324212f4a54263ef4b827c0a
Ken Lau <ksla@deakin.edu.au>
2020-11-24 15:09:15 +1100

    Add Button Animation
    Add Button Animation Only for Enter AR Scene Button

--- AR-Geolocation-App-Unity/Assets/_Scripts/AudioManager.cs
+++ AR-Geolocation-App-Unity/Assets/_Scripts/AudioManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
@@ -9,11 +11,18 @@ public class AudioManager : MonoBehaviour
     public AudioSource audioSource2;
     public AudioSource audioSource3;
     public AudioSource OpenSFX;
+    public float animationSpeed;
+
+    private Transform btnReturnToMapTransform;
+    private Transform btnObjATransform;
+    private float animationDelay = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
         OpenSFX.Play();
         Invoke("BGM", 2);
+        btnReturnToMapTransform = GameObject.Find("ReturnToMap").GetComponent<Transform>();
+        //btnObjATransform = GameObject.Find("ObjA").GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -40,6 +49,35 @@ public class AudioManager : MonoBehaviour
     public void BGM()
     {
         bgm.Play();
+    }
+
+    public void animation(Transform btnTransform)
+    {
+        btnTransform.localScale -= new Vector3(0.4f, 0.4f, 0);
+        Invoke("finishAnimate", animationSpeed);
+    }
+
+    public void finishAnimate()
+    {
+       btnReturnToMapTransform.localScale = new Vector3(1, 1, 1);
+    }
+
+    public void buttonOnClick(string buttonName)
+    {
+        switch (buttonName)
+        {
+        case "ReturnToMap":    
+            animation(btnReturnToMapTransform);
+            Invoke("EnterARScene", animationSpeed + animationDelay);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void EnterARScene()
+    {
+        SceneManager.LoadScene("MapSceneLight", LoadSceneMode.Single);
     }
 
   
