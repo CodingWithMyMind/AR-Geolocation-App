faaa6a3ea99fb41b8411a2cfec6b27caa60a8178
Ben <monoganog@gmail.com>
2020-12-14 09:52:21 +1100

    edits

--- AR-Geolocation-App-Unity/Assets/_Scripts/GameManager.cs
+++ AR-Geolocation-App-Unity/Assets/_Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -147,6 +148,7 @@ public class GameManager : MonoBehaviour
     {
         TurnOnUI(ARModeUI,ARModeNavUI);
 
+        SceneManager.LoadSceneAsync("ARScene1");
 
         state = State.ARMode;
     }
